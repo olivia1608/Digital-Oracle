44ff35d4348557fc658515e2bb8d045b6ab17d7e
olivia cahyani agustina <oliviacahyania@gmail.com>
2025-05-03 10:54:10 +0700

    membenarkan tampilan di beranda

--- admin/t_kategori.php
+++ admin/t_kategori.php
@@ -1,3 +1,27 @@
+<?php
+include "koneksi.php";
+
+if (isset($_POST["simpan"])) {
+  $auto = mysqli_query($koneksi, "select max(id_kategori) as max_code from tb_kategori");
+  $hasil = mysqli_fetch_array($auto);
+  $code = $hasil["max_code"];
+  $urutan = (int)substr($code, 1, 3);
+  $urutan++;
+  $huruf = "K";
+  $id_kategori = $huruf . sprintf("%03s", $urutan);
+  $nm_kategori = $_POST['nm_kategori'];
+
+  $query = mysqli_query($koneksi, "INSERT INTO tb_kategori(id_kategori, nm_kategori) VALUES ('$id_kategori', '$nm_kategori')");
+  if ($query) {
+    echo "<script>alert('Data berhasil ditambahkan!')</script>";
+    header("refresh:0, kategori.php");
+  } else {
+    echo "<script>alert('Data gagal ditambahkan!')</script>";
+    header("refresh:0, kategori.php");
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +29,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Beranda - digitaloracle Admin</title>
+  <title>t_kategori - DigitalOracle Admin</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -51,73 +75,81 @@
       <i class="bi bi-list toggle-sidebar-btn"></i>
     </div><!-- End Logo -->
 
-
     <nav class="header-nav ms-auto">
       <ul class="d-flex align-items-center">
 
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow messages">
+        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
           <li class="dropdown-header">
-            You have 3 new messages
+            You have 4 new notifications
             <a href="#"><span class="badge rounded-pill bg-primary p-2 ms-2">View all</span></a>
           </li>
           <li>
             <hr class="dropdown-divider">
           </li>
 
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-1.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>Maria Hudson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>4 hrs. ago</p>
-              </div>
-            </a>
+          <li class="notification-item">
+            <i class="bi bi-exclamation-circle text-warning"></i>
+            <div>
+              <h4>Lorem Ipsum</h4>
+              <p>Quae dolorem earum veritatis oditseno</p>
+              <p>30 min. ago</p>
+            </div>
           </li>
+
           <li>
             <hr class="dropdown-divider">
           </li>
 
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-2.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>Anna Nelson</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>6 hrs. ago</p>
-              </div>
-            </a>
+          <li class="notification-item">
+            <i class="bi bi-x-circle text-danger"></i>
+            <div>
+              <h4>Atque rerum nesciunt</h4>
+              <p>Quae dolorem earum veritatis oditseno</p>
+              <p>1 hr. ago</p>
+            </div>
           </li>
+
           <li>
             <hr class="dropdown-divider">
           </li>
 
-          <li class="message-item">
-            <a href="#">
-              <img src="assets/img/messages-3.jpg" alt="" class="rounded-circle">
-              <div>
-                <h4>David Muldon</h4>
-                <p>Velit asperiores et ducimus soluta repudiandae labore officia est ut...</p>
-                <p>8 hrs. ago</p>
-              </div>
-            </a>
+          <li class="notification-item">
+            <i class="bi bi-check-circle text-success"></i>
+            <div>
+              <h4>Sit rerum fuga</h4>
+              <p>Quae dolorem earum veritatis oditseno</p>
+              <p>2 hrs. ago</p>
+            </div>
           </li>
+
           <li>
             <hr class="dropdown-divider">
           </li>
 
+          <li class="notification-item">
+            <i class="bi bi-info-circle text-primary"></i>
+            <div>
+              <h4>Dicta reprehenderit</h4>
+              <p>Quae dolorem earum veritatis oditseno</p>
+              <p>4 hrs. ago</p>
+            </div>
+          </li>
+
+          <li>
+            <hr class="dropdown-divider">
+          </li>
           <li class="dropdown-footer">
-            <a href="#">Show all messages</a>
+            <a href="#">Show all notifications</a>
           </li>
 
-        </ul><!-- End Messages Dropdown Items -->
+        </ul><!-- End Notification Dropdown Items -->
 
-        </li><!-- End Messages Nav -->
+        </li><!-- End Notification Nav -->
 
         <li class="nav-item dropdown pe-3">
 
           <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="assets/img/Foto.jpg" alt="Profile" class="rounded-circle">
+            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
           </a><!-- End Profile Iamge Icon -->
 
           <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
@@ -125,10 +157,6 @@
               <h6>Olivia</h6>
               <span>Admin</span>
             </li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-
             <li>
               <hr class="dropdown-divider">
             </li>
@@ -154,16 +182,16 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="index.php">
+        <a class="nav-link collapsed" href="index.html">
           <i class="bi bi-grid"></i>
           <span>Beranda</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="kategori.php">
+        <a class="nav-link collapsed" href="kategori produk.php">
           <i class="bi bi-person"></i>
-          <span>Kategori</span>
+          <span>Kategori Produk</span>
         </a>
       </li><!-- End Kategori Page Nav -->
 
@@ -209,75 +237,42 @@
   <main id="main" class="main">
 
     <div class="pagetitle">
-      <h1>Beranda</h1>
+      <h1>Kategori Produk</h1>
       <nav>
         <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.php">Beranda</a></li>
-          <li class="breadcrumb-item active">Beranda</li>
+          <li class="breadcrumb-item"><a href="index.html">Beranda</a></li>
+          <li class="breadcrumb-item">Kategori Produk</li>
+          <li class="breadcrumb-item active">Tambah</li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
 
-    <section class="section dashboard">
+    <section class="section">
       <div class="row">
-
-        <!-- Left side columns -->
-        <div class="col-lg-8">
-          <div class="row">
-
-            <!-- Welcome Card -->
-            <div class="col-12">
-              <div class="card info-card customers-card shadow-sm w-100">
-                <div class="card-body text-center py-4">
-                  <h4 class="mb-2">Selamat datang di Website Admin <strong>Digitaloracle!</strong></h4>
-                  <p class="text-muted small mb-0">Kelola produk, transaksi, dan pelanggan dengan mudah.</p>
+        <div class="col-lg-6">
+          <div class="card">
+            <div class="card-body">
+              <form class="row g-3 mt-2" method="post">
+                <div class="col-12">
+                  <label for="nm_kategori"
+                  class="form-label">Nama Kategori</label>
+                  <input type="text" class="form-control"
+                  id="nm_kategorii" name="nm_kategori"
+                  placeholder="Masukkan Nama Kategori Produk">
                 </div>
-              </div>
-            </div>
-            <!-- End Welcome Card -->
-            <!-- Orders Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card sales-card">
-
-                <div class="card-body">
-                  <h5 class="card-title">Pesanan <span>| Semua Waktu</span></h5>
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-basket"></i> <!-- Ikon keranjang belanja -->
-                    </div>
-                    <div class="ps-3">
-                      <h6>0</h6>
-                    </div>
-                  </div>
+                <div class="text-center">
+                  <button type="reset" class="btn
+                  btn-secondary">Reset</button>
+                  <button type="submit" class="btn btn-primary"
+                  name="simpan">Simpan</button>
                 </div>
-              </div>
+              </form>
+
             </div>
-            <!-- End Orders Card -->
-            <!-- Revenue Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card revenue-card">
-
-                <div class="card-body">
-                  <h5 class="card-title">Pendapatan <span>| Hari ini</span></h5>
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>Rp. 0</h6>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-            <!-- End Revenue Card -->
           </div>
-        </div><!-- End Left side columns -->
-      </div>
+        </div>
+      </div>        
     </section>
-
 
   </main><!-- End #main -->
 
@@ -291,7 +286,7 @@
       <!-- You can delete the links only if you purchased the pro version. -->
       <!-- Licensing information: https://bootstrapmade.com/license/ -->
       <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://instagram.com/olivia_chyn/" target="_blank">OliviaCahyaniAgustina</a>
+      Designed by <a href="https://instagram.com/olicia_chyn/" target="_blank">OliviaCahyaniAgustina</a>
     </div>
   </footer><!-- End Footer -->
 
