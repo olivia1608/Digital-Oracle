7bd1bb7a5c524f51dab34d84ffcd4db0c7f4b29d
olivia cahyani agustina <oliviacahyania@gmail.com>
2025-06-09 17:12:18 +0700

    membenarkan tampilan di belanja.php, dan detail_produk.php

--- belanja.php
+++ belanja.php
@@ -825,7 +825,7 @@ WHERE p.id_user = '$id_user'
                                         <div class="single-add-to-cart">
                                             <form action="tambah_ke_keranjang.php" method="POST" class="cart-quantity">
                                                 <input type="hidden" name="id_produk" id="input-id-produk">
-                                                <input type="hidden" name="id_user" value="<?= $_SESSION['id_user'] ?>">
+                                                <input type="hidden" name="id_user" value="<?= isset($_SESSION['id_user']) ? $_SESSION['id_user'] : '' ?>">
                                                 <input type="hidden" name="harga" id="input-harga">
                                                 <input type="hidden" name="redirect_url" value="belanja.php">
 
